74be46526b8db0a2799ab0f97f865ea16a775db7
Neil Elkadi <elkadineil@gmail.com>
2022-08-14 02:46:56 -0400

    made app.js a component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
 import { Link } from "react-router-dom";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>qBitAutoTorrent Main Page</h1>
-        <nav
-          style={{
-            borderBottom: "solid 1px",
-            paddingBottom: "1rem",
-          }}
-        >
-          <Link to="/login">login</Link> |{" "}
-          <Link to="/auto">Auto Torrent</Link>
-        </nav>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h1>qBitAutoTorrent Main Page</h1>
+          <nav
+            style={{
+              borderBottom: "solid 1px",
+              paddingBottom: "1rem",
+            }}
+          >
+            <Link to="/login">login</Link> |{" "}
+            <Link to="/auto">Auto Torrent</Link>
+          </nav>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
