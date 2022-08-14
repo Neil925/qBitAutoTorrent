e16670e508a292825a5ae47ca0e6fdc2b918f8ce
Damien <DamienB2474@protonmail.com>
2022-08-14 04:32:40 -0400

    styling to login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,40 +5,42 @@ import Form from "react-bootstrap/Form";
 import "./login.css";
 
 class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
 
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        });
-    }
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group className="mb-3" >
-          <Form.Label>username</Form.Label>
-          <Form.Control type="text" placeholder="username" name="username" onChange={this.onChange}/>
-        </Form.Group>
-        <Form.Group className="mb-3" >
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="password" name="password" onChange={this.onChange}/>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <div className="main">
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Group>
+                  <Form.Label>username</Form.Label>
+                  <Form.Control type="text" placeholder="username" name="username" onChange={this.onChange} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="password" name="password" onChange={this.onChange} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </div>
     );
   }
 }
