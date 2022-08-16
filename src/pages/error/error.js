2bf56cc3a860becc5fe4c0183605a16699ae833b
Cookies <teamtyler2@gmail.com>
2022-08-16 00:50:50 -0600

    Updated Error404 &  SCSS

--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -1,31 +1,21 @@
 // import React, {useState} from "react";
 import React, { Component } from "react";
-import { Alert } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import "./error.scss";
 
 class Error404 extends Component {
   render() {
     return (
-      <div className="Error" >
-      <header className="Error-header">
-      <Alert variant="success">
-      <Alert.Heading><h1>Error 404!</h1>
-      </Alert.Heading>
-      </Alert>
-      </header>
-      <body className="below">
-      <p>
-      <nav
-            style={{
-              borderBottom: "solid 3px",
-              paddingBottom: "-5",
-            }}
-          >
-          </nav>
-       </p>
-       <b>Go back you normie!</b>
-       </body>
-    </div>
+      <div className="error">
+        <Card className="error text-center">
+          <Card.Header>Error 404</Card.Header>
+          <Card.Body>
+            <Card.Title>You did the big funny!</Card.Title>
+          </Card.Body>
+          <Button variant="primary">Go back to homepage!</Button>
+        </Card>
+      </div>
     );
   }
 }
