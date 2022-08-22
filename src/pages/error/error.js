e6a605286a2d844569294bd4e994a8721ed871f6
Damien <DamienB2474@protonmail.com>
2022-08-22 02:49:46 -0400

    made 404 btn redirect
    made 404 btn redirect to "/"

--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -1,23 +1,28 @@
 // import React, {useState} from "react";
-import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import "./error.scss";
+import { useNavigate } from 'react-router';
 
-class Error404 extends Component {
-  render() {
-    return (
-      <div className="error">
-        <Card className="error text-center">
-          <Card.Header>Error 404</Card.Header>
-          <Card.Body>
-            <Card.Title>You did the big funny!</Card.Title>
-          </Card.Body>
-          <Button variant="primary">Go back to homepage!</Button>
-        </Card>
-      </div>
-    );
+function Error404() {
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate('/')
   }
+  return(
+    <div className="error">
+      <Card className="error text-center">
+        <Card.Header>Error 404</Card.Header>
+        <Card.Body>
+          <Card.Title>You did the big funny!</Card.Title>
+        </Card.Body>
+        <Button variant="primary" onClick={handleClick}>
+          Go back to homepage!
+        </Button>
+      </Card>
+    </div>
+  );
 }
 
 export default Error404;
